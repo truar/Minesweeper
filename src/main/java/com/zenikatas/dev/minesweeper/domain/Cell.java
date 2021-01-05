c4557d185c089d69915577f47a2d35b189df7a75
Romain Vernoux <romain.vernoux@zenika.com>
2021-01-06 00:04:58 +0100

    Annotations and comments

--- src/main/java/com/zenikatas/dev/minesweeper/domain/Cell.java
+++ src/main/java/com/zenikatas/dev/minesweeper/domain/Cell.java
@@ -2,9 +2,9 @@ package com.zenikatas.dev.minesweeper.domain;
 
 public class Cell {
     private CellValue value;
-    private boolean isUncovered = false;
+    private boolean isUncovered = false; // TODO uncovered?
 
-    public Cell(String initialValue) {
+    public Cell(String initialValue) { // TODO why not pass the CellValue here?
         this.value = CellValue.of(initialValue);
     }
 
@@ -14,10 +14,10 @@ public class Cell {
 
     public boolean isCovered() {
         return !isUncovered;
-    }
+    } // TODO why two methods?
 
     public void uncover(int countNumberOfAdjacentMine) {
-        value = CellValue.of(countNumberOfAdjacentMine);
+        value = CellValue.of(countNumberOfAdjacentMine); // TODO it troubles me that the outside world tells what to display inside the cell. This should be encapsulated in the CellValue somehow?
         isUncovered = true;
     }
 
@@ -27,7 +27,7 @@ public class Cell {
 
     public boolean isAMine() {
         return value == CellValue.MINED;
-    }
+    } // TODO can we encapsulate the caller behavior inside CellValue ?
 
     public CellValue value() {
         return value;
--- src/test/java/com/zenikatas/dev/minesweeper/domain/RandomGridGeneratorTest.java
+++ src/test/java/com/zenikatas/dev/minesweeper/domain/RandomGridGeneratorTest.java
@@ -8,7 +8,7 @@ class RandomGridGeneratorTest {
 
     @Test
     void generates_random_grid() {
-        for (int row = 2; row <= 10; row++) {
+        for (int row = 2; row <= 10; row++) { // TODO tests in a loop? Please, this is not Golang...
             for (int col = 2; col <= 10; col++) {
                 for (int mineNumber = 1; mineNumber < (row * col) / 2; mineNumber++) {
                     String[][] grid = new RandomGridGenerator().generate(row, col, mineNumber);
